190703e1c9fda3c362e88877dc18f6b954e28630
Michael Hopper <mike.hopper@ga.co>
2014-12-12 12:32:11 -0500

    More CSS and images.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,34 +1,26 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name jeopardyApp
- * @description
- * # jeopardyApp
- *
- * Main module of the application.
- */
 angular
-  .module('jeopardyApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.module('jeopardyApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngMessages',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+});
